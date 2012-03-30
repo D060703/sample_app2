9fa2643dac67818984be661a98bed5caa9125aef
David <dmok21@gmail.com>
2012-03-30 02:17:38 -0400

    Removed default Rails page

--- Gemfile
+++ Gemfile
@@ -25,6 +25,10 @@ gem 'rspec-rails', '2.6.1'
 gem 'webrat', '0.7.1' 
 gem 'spork', '0.9.0.rc8' 
 gem 'factory_girl_rails', '1.0'
+gem 'autotest','4.4.6'
+gem 'autotest-rails-pure', '4.1.2'
+gem 'autotest-fsevent', '0.2.4' 
+gem 'autotest-growl', '0.2.16'
 
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 SampleApp::Application.routes.draw do
+  
+  match '/contact', :to => 'pages#contact'
+  match '/about', :to => 'pages#about'
+  match '/help', :to => 'pages#help'
+  
+  root :to => 'pages#home'
+  root_path => '/' 
+  root_url => 'http://localhost:3000/'
+  
   get "pages/home"
   get "pages/about"
   get "pages/contact"
+  get "pages/help"
+  
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
